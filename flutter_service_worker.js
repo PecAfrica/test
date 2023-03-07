a179d1be4bfc0d52bc08a3e4fb9072c0bdc5f515
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-03-08 00:40:56 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "9caf46eccb99615c5520dcb63e1e453e",
+  "assets/AssetManifest.json": "b29b97c9ddb7584c206e8d0dbf81c44e",
 "assets/assets/fonts/Poppins/Poppins-Bold.ttf": "08c20a487911694291bd8c5de41315ad",
 "assets/assets/fonts/Poppins/Poppins-Regular.ttf": "093ee89be9ede30383f39a899c485a82",
 "assets/assets/fonts/Poppins/Poppins-SemiBold.ttf": "6f1520d107205975713ba09df778f93f",
+"assets/assets/images/construction.jpg": "7ac19e232c748168abb181264890415c",
 "assets/assets/images/logo.jpg": "4e3407c919c4325174a62cee1ec125ef",
 "assets/FontManifest.json": "feb445ba054bf18e36b2b18df7691723",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
@@ -22,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "21db2a9f402f144355b83863e98f2178",
 "icons/Icon-maskable-192.png": "21db2a9f402f144355b83863e98f2178",
 "icons/Icon-maskable-512.png": "21db2a9f402f144355b83863e98f2178",
-"index.html": "ebbd41f9afda4fbd61497bf8b3182af0",
-"/": "ebbd41f9afda4fbd61497bf8b3182af0",
-"main.dart.js": "4fd1c0b250b271a61f1ba2a55f2bd71e",
+"index.html": "83e6668bef5300d87b2d29768c3e0a22",
+"/": "83e6668bef5300d87b2d29768c3e0a22",
+"main.dart.js": "8c5ccd7ece90929fd4c3a1d47acf1bb1",
 "manifest.json": "b7cac397d7988ac725161a5b14bc92e4",
 "version.json": "a94cf47ce8ef00756818d0e641035f19"
 };
